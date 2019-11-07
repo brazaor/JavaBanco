4c585173774b657d44860ada1f8dcd40b284ef1e
Rafael Brazao <brazaor@gmail.com>
2019-11-07 19:10:46 -0300

    incluindo metodos para acessar o banco de dados

--- src/br/edu/usj/javabanco/dao/Conexao.java
+++ src/br/edu/usj/javabanco/dao/Conexao.java
@@ -1,11 +1,23 @@
 package br.edu.usj.javabanco.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
 
 	public static Connection getConexao() {
 		Connection conexao = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexao = DriverManager.getConnection("jdbc:mysql://localhost/java_banco", "root", "rafael00");
+		} catch (ClassNotFoundException e1) {
+			System.err.println("Driver não encontrado!");
+			e1.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("Erro ao conectar ao banco de dados!");
+			e.printStackTrace();
+		}
 		return conexao;
 	}
 }
--- src/br/edu/usj/javabanco/dao/EmpresaDAO.java
+++ src/br/edu/usj/javabanco/dao/EmpresaDAO.java
@@ -1,5 +1,10 @@
 package br.edu.usj.javabanco.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,16 +14,63 @@ public class EmpresaDAO {
 
 	public List<Empresa> listarEmpresas(){
 		List<Empresa> empresas = new ArrayList<Empresa>();
+		Connection conexao = Conexao.getConexao();
+		try {
+			Statement stm = conexao.createStatement();
+			String sql = "SELECT * FROM EMPRESA";
+			ResultSet rs = stm.executeQuery(sql);
+			
+			while(rs.next()){
+				Empresa empresa = new Empresa();
+				empresa.setId(rs.getInt("id"));
+				empresa.setNome(rs.getString("nome"));
+				empresa.setRamoAtuacao(rs.getString("ramo_atuacao"));
+				empresa.setAnoFundacao(rs.getInt("ano_fundacao"));
+				empresa.setCapital(rs.getFloat("capital"));
+				
+				empresas.add(empresa);
+			}
+			
+		} catch (SQLException e) {
+			System.err.println("Erro ao listar as empresas!");
+			e.printStackTrace();
+		}
+ 
 		return empresas;
 	}
 	
 	public boolean gravarEmpresa(Empresa e) {
 		int resultado = 0;
+		Connection conexao = Conexao.getConexao();
+		String sql = "INSERT INTO EMPRESA VALUES(?,?,?,?,?)";
+		try {
+			PreparedStatement stm = conexao.prepareStatement(sql);
+			stm.setInt(1, e.getId());
+			stm.setString(2, e.getNome());
+			stm.setInt(3, e.getAnoFundacao());
+			stm.setString(4, e.getRamoAtuacao());
+			stm.setFloat(5, e.getCapital());
+			
+			resultado = stm.executeUpdate();
+		} catch (SQLException e1) {
+			System.err.println("Erro ao gravar a empresa!");
+			e1.printStackTrace();
+		}
 		return resultado > 0;
 	}
 	
 	public boolean excluirEmpresa(int codigo) {
 		int resultado = 0;
+		Connection conexao = Conexao.getConexao();
+		String sql = "DELETE FROM EMPRESA WHERE id = ?";
+		try {
+			PreparedStatement stm = conexao.prepareStatement(sql);
+			stm.setInt(1, codigo);
+			resultado = stm.executeUpdate();
+		} catch (SQLException e1) {
+			System.err.println("Erro ao excluir a empresa!");
+			e1.printStackTrace();
+		}
 		return resultado > 0;
 	} 
 }
